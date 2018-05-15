df7125a48d4ae4864f72519edd0e5e49e3708093
Damien Zufferey <zufferey@mpi-sws.org>
2018-05-15 17:36:37 +0200

    small stuff

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ organization := "io.github.dzufferey"
 
 version := "0.1-SNAPSHOT"
 
-scalaVersion := "2.12.4"
+scalaVersion := "2.12.6"
 
 scalacOptions in Compile ++= Seq(
     //"-Xlog-implicits",
--- list_occ_jar.sh
+++
@@ -1,8 +0,0 @@
-#~/bin/sh
-
-jar tf lib/occjava.jar | grep class | sed 's/\//./g' | sed 's/\.class//' | while read class; do
-  echo -n //
-  javap -classpath lib/occjava.jar -public $class
-  echo
-  echo
-done
